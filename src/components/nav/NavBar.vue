2b528e405a885684bc9b783a85d3738bc32ee81e
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2023-12-28 15:50:33 +0500

    Добавил поиск

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -2,19 +2,36 @@
 
 import SiteButton from "@/components/SiteButton.vue";
 import ProfileIcon from "@/components/icons/ProfileIcon.vue";
+import SearchInput from "@/components/nav/SearchInput.vue";
 </script>
 
 <template>
   <nav class="navbar navbar-dark bg-black">
     <div class="container">
-      <RouterLink :to="{name: 'home'}" class="navbar-brand" href="/">
-        <img src="../../assets/image/logo.svg" width="24" height="24" class="d-inline-block align-top"
-             alt="Логотип Брусники">
-        <span class="navbar-title">База знаний</span>
-      </RouterLink>
-      <div class="right-block">
-        <SiteButton><span class="mx-3">Вход</span></SiteButton>
-        <a href="#" class="ms-4"><ProfileIcon /></a>
+      <div class="row w-100 justify-content-between">
+        <!-- Logo -->
+        <div class="col col-auto">
+          <RouterLink :to="{name: 'home'}" class="navbar-brand" href="/">
+            <img src="../../assets/image/logo.svg" width="24" height="24" class="d-inline-block align-top"
+                 alt="Логотип Брусники">
+            <span class="navbar-title">База знаний</span>
+          </RouterLink>
+        </div>
+
+        <!-- Search -->
+        <div class="col col-6">
+          <SearchInput class="w-100"/>
+        </div>
+
+        <!-- Auth Button and Profile Icon -->
+        <div class="col col-auto">
+          <div class="d-flex">
+            <SiteButton><span class="mx-3">Вход</span></SiteButton>
+            <a href="#" class="ms-4">
+              <ProfileIcon/>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </nav>
@@ -25,10 +42,14 @@ import ProfileIcon from "@/components/icons/ProfileIcon.vue";
   display: flex;
   align-items: center;
 }
+
 .navbar-title {
   margin-left: 10px;
 }
-.right-block {
+
+.col {
   display: flex;
+  align-items: center;
 }
+
 </style>
---
+++ src/components/nav/SearchInput.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+import SearchIcon from "@/components/icons/SearchIcon.vue";
+</script>
+
+<template>
+  <form class="form-inline">
+    <div class="input-group">
+      <span class="input-group-text" id="search-icon"><SearchIcon/></span>
+      <input type="text"
+             class="form-control shadow-none"
+             placeholder="Поиск"
+             aria-label="Search"
+             aria-describedby="search-icon">
+    </div>
+  </form>
+</template>
+
+<style scoped>
+.input-group-text {
+  background-color: white;
+  border-right: 0;
+  border-bottom-left-radius: 30px;
+  border-top-left-radius: 30px;
+}
+
+.form-control {
+  border-left: 0;
+  border-bottom-right-radius: 30px;
+  border-top-right-radius: 30px;
+}
+</style>
